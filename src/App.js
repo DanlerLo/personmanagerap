76c450241e4c18a43bafbb81d45b4f6efccc68b6
DanlerLo <loaizad65@gmail.com>
2022-05-24 20:10:12 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { Layout } from './Layout';
+import { NoMatch } from './components/NoMatch';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { List, People } from './components/People';
+import { NavigationBar } from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Routes>
+              <Route path="/" element={<List />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="People" element={<List />}  />
+              <Route path="Login" element={<Login />} />
+              <Route element={<NoMatch/>} />
+            </Routes>
+          </Router>
+        </Layout>
+      </React.Fragment>
     </div>
   );
 }
